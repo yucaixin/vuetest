42aeb2835297ad7b6d96bede24107698e84559de
yucaixin <1487538550@qq.com>
2019-05-18 10:21:42 +0800

    sss

--- mytest/src/App.vue
+++ mytest/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+    
   </div>
 </template>
 
---
+++ mytest/src/views/Mine/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>MI</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
